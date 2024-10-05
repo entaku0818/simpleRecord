b134aab4d6184be3ca09983b48ec64327673e693
entaku <entaku19890818@gmail.com>
2024-10-05 22:02:17 +0900

    リストのデータを生成

--- app/src/main/java/com/entaku/simpleRecord/RecordingsScreen.kt
+++ app/src/main/java/com/entaku/simpleRecord/RecordingsScreen.kt
@@ -1,16 +1,63 @@
 package com.entaku.simpleRecord
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
+// 録音データを持つデータクラス
+data class RecordingData(
+    val title: String,
+    val creationDate: LocalDateTime,
+    val fileExtension: String,
+    val khz: String,
+    val bitRate: Int,
+    val channels: Int,
+    val duration: String,
+    val filePath: String
+)
+
 @Composable
 fun RecordingsScreen(onNavigateToRecordScreen: () -> Unit) {
-    var recordings by remember { mutableStateOf(List(10) { "Recording ${it + 1}" }) }
+    var recordings by remember {
+        mutableStateOf(
+            List(20) { // リストの件数を増やしました
+                RecordingData(
+                    title = "Recording ${it + 1}",
+                    creationDate = LocalDateTime.now().minusDays(it.toLong()),
+                    fileExtension = "wav",
+                    khz = "44",
+                    bitRate = 16,
+                    channels = 2,
+                    duration = "00:0${it + 1}:30",
+                    filePath = "/path/to/recording${it + 1}.wav"
+                )
+            }
+        )
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -21,7 +68,7 @@ fun RecordingsScreen(onNavigateToRecordScreen: () -> Unit) {
             contentPadding = PaddingValues(16.dp)
         ) {
             items(recordings) { recording ->
-                RecordingItem(recording)
+                RecordingListItem(recording)
                 Spacer(modifier = Modifier.height(8.dp))
             }
         }
@@ -40,16 +87,73 @@ fun RecordingsScreen(onNavigateToRecordScreen: () -> Unit) {
 }
 
 @Composable
-fun RecordingItem(recording: String) {
+fun RecordingListItem(recording: RecordingData) {
+    val formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd")
+    val formattedDate = recording.creationDate.format(formatter)
+
     Card(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth(),
     ) {
-        Text(
-            text = recording,
-            modifier = Modifier.padding(16.dp)
-        )
+        Column(modifier = Modifier.padding(16.dp)) {
+
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = recording.title,
+                    style = MaterialTheme.typography.bodyLarge,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+
+                Text(
+                    text = recording.duration,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+
+            Spacer(modifier = Modifier.height(1.dp))
+
+            // 録音時間、ファイルの情報を表示
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                Text(
+                    text = formattedDate,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Row(){
+                    Text(
+                        text = "${recording.khz} kHz",
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                    Text(text = "/")
+                    Text(
+                        text = "${recording.bitRate} bit",
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                    Text(text = "/")
+                    Text(
+                        text = "${recording.channels} ch",
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                }
+
+
+                Text(
+                    text = recording.fileExtension.uppercase(),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+
+        }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
